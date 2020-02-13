03234c4904ebe6934d3a4c2a8d3b0300b8e4b88f
Derek Wang <guanghuiw@vmware.com>
2020-02-13 17:06:02 +0800

    remove useless route and page

--- src/pages/RouteConfig.js
+++ src/pages/RouteConfig.js
@@ -5,7 +5,6 @@ import PrivateRoute from './PrivateRoute';
 
 import Home from './Home';
 import Category from './Category';
-import SearchWithInputPage from './SearchWithInputPage';
 import User from './User';
 import _404 from './404';
 import Login from './Login';
@@ -23,9 +22,8 @@ function RouteConfig(props) {
       <Route exact path='/' component={Home} onEnter={setTitle('多快好省，购物上京东！')}></Route>
       <Route exact path='/home' component={Home} onEnter={setTitle('多快好省，购物上京东！')}></Route>
       <PrivateRoute path='/user' component={User} onEnter={setTitle('用户中心 - 京东商城')} />
-      <Route path='/products' component={Products}></Route>
+      <Route path='/products' component={Products}></Route> {/* search products */}
       <Route path='/category' component={Category} onEnter={setTitle('京东商品分类 - 京东商城')}></Route>
-      <Route path='/searchWithInputPage' component={SearchWithInputPage} />
       <Route path='/login' component={Login} onEnter={setTitle('京东登录')}></Route>
       <Route component={_404} onEnter={setTitle('404')}></Route>
     </Switch>
--- src/pages/SearchWithInputPage/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function SearchWithInputPage() {
-  return <div>SearchWithInputPage</div>;
-}
